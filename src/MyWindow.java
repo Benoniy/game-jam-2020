7a263ed12807b63147a6a80a8d09b18204ac91c4
br17011 <br17011@essex.ac.uk>
2020-01-31 18:01:05 +0000

    base class creation

---
+++ src/MyWindow.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+
+public class MyWindow extends JFrame {
+    public MyWindow(){
+        setTitle("");
+        setMinimumSize();
+        setMaximumSize();
+        setPreferredSize();
+    }
+}
