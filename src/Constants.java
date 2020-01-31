04345244efa29274505219b34c358fd3bc548810
br17011 <br17011@essex.ac.uk>
2020-01-31 19:56:37 +0000

    added salvaged game object class from a previous project

--- src/Constants.java
+++ src/Constants.java
@@ -4,4 +4,7 @@ public class Constants {
     static int width = 1280;
     static int height = 720;
     static Dimension screenDimension = new Dimension(width, height);
+
+    public static final int DELAY = 20;  // in milliseconds
+    public static final double DT = DELAY / 1000.0;  // in seconds
 }
---
+++ src/GameObject.java
@@ -0,0 +1,61 @@
+import Resources.Vector2D;
+
+import java.awt.*;
+
+public abstract class GameObject {
+    public Vector2D position;
+    Vector2D velocity;
+    double radius;
+    boolean dead;
+    boolean god = false;
+    double godTime;
+
+
+
+    public GameObject(Vector2D position, Vector2D velocity, double radius){
+        this.position = position;
+        this.velocity = velocity;
+        this.radius = radius;
+        this.dead = false;
+    }
+
+    public void hit(){
+        dead = true;
+    }
+
+
+
+    public boolean overlap(GameObject other){
+        if (position.dist(other.position) < radius + other.radius) {
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+    public boolean spawnOverlap(GameObject other){
+        if (position.dist(other.position) < radius + other.radius + 50) {
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+    public void collisionHandling(GameObject other){
+        if (this.overlap(other)){
+            this.hit();
+            other.hit();
+        }
+    }
+
+    public void update(){
+        position.addScaled(velocity, Constants.DT);
+        position.wrap(Constants.width, Constants.height);
+    }
+
+    public boolean canHit(GameObject other){return true;};
+
+    public abstract void draw(Graphics2D g);
+}
