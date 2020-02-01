c51c8a27e4aea206f45d8af9c4b94917d4cfa600
Henry <henryrlewis@btinternet.com>
2020-02-01 16:30:06 +0000

    Body Parts update.

--- src/Sprites.java
+++ src/Sprites.java
@@ -3,7 +3,7 @@ import Resources.ImageManager;
 import java.awt.*;
 
 public class Sprites {
-    public static Image Wall1, Floor1, Crate1, WallEnd, WallCor, WallMid;
+    public static Image Wall1, Floor1, Crate1, WallEnd, WallCor, WallMid, head, arm, legs, chest;
     static {
         try{
             Wall1 = ImageManager.loadImage("wall");
@@ -12,6 +12,11 @@ public class Sprites {
             WallMid = ImageManager.loadImage("wallMid");
             Floor1 = ImageManager.loadImage("floor");
             Crate1 = ImageManager.loadImage("crate");
+            // body parts
+            head = ImageManager.loadImage("head");
+            arm = ImageManager.loadImage("arm");
+            legs = ImageManager.loadImage("legs");
+            chest = ImageManager.loadImage("chest");
         }
         catch (Exception e){
             e.printStackTrace();
