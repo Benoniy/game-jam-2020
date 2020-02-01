7abb3a2d34872e09f556944e77d8b8e2e494d38d
Henry <henryrlewis@btinternet.com>
2020-02-01 11:20:31 +0000

    Added buffered-image read-in code for wall texture.

--- src/WallObject.java
+++ src/WallObject.java
@@ -1,4 +1,8 @@
+import javax.imageio.ImageIO;
 import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.net.URL;
 
 public class WallObject extends EnviroObject {
     public WallObject(int posX, int posY) {
@@ -7,9 +11,20 @@ public class WallObject extends EnviroObject {
     }
     @Override
     public void draw(Graphics2D g) {
+
+        // Buffered Image is the texture
+        BufferedImage texture = null;
+        try {
+            URL url = new URL("assets/wall.png");
+            texture = ImageIO.read(url);
+        } catch (IOException e) {
+
+        }
+
+
         g.setColor(Color.BLUE);
         g.fillRect((int) position.x, (int)position.y, sizeX, sizeY);
-        g.setColor(Color.black);
         g.drawRect((int) position.x, (int)position.y, sizeX, sizeY);
     }
+
 }
