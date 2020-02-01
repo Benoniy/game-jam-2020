90406c1c6b3e08105b4293d6085142e60bd951a1
br17011 <br17011@essex.ac.uk>
2020-02-01 16:34:23 +0000

    added texture rotation offset fix

--- src/EnviroObject.java
+++ src/EnviroObject.java
@@ -5,18 +5,20 @@ import java.awt.geom.AffineTransform;
 
 public abstract class EnviroObject extends GameObject {
     int offsetX, offsetY;
+
     double theta = 0;
     Vector2D offset;
     Image texture = null;
     AffineTransform spriteAffine;
 
 
-    public EnviroObject(int posX, int posY, double radius, boolean collision, Image INtexture, double theta){
+    public EnviroObject(int posX, int posY, double radius, boolean collision, Image INtexture, int theta){
         super(new Vector2D(Constants.translate(posX), Constants.translate(posY)), new Vector2D(0,0), radius, collision);
         this.offsetX = Constants.translate(posX);
         this.offsetY = Constants.translate(posY);
         this.offset = new Vector2D(offsetX, offsetY);
         this.theta = theta;
+
         if (INtexture != null){
             this.texture = INtexture;
             genSpriteAffine();
@@ -30,9 +32,20 @@ public abstract class EnviroObject extends GameObject {
         double stretchY = ((this.radius * 2) /TxHeight);
 
         spriteAffine = new AffineTransform();
-        spriteAffine.rotate( theta);
-        spriteAffine.scale(stretchX, stretchY);
 
+        if (theta != 0){
+            for (int i = 0; i < theta / 90; i ++){
+                AffineRotate90(TxWidth, TxHeight);
+            }
+        }
+
+        spriteAffine.scale(stretchX, stretchY);
+    }
+
+    public void AffineRotate90(double width, double height){
+        spriteAffine.rotate(Math.PI/2, width / 2, height /2);
+        double offset = (width - height)/2;
+        spriteAffine.translate(-offset, -offset);
     }
 
     @Override
